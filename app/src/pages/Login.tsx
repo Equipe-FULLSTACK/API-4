b22ac32452bd18da428dbbfaeeb776dfc2ff6aec
Willians Motta <112169854+williansmott4@users.noreply.github.com>
2024-04-06 15:17:11 -0300

    Link css

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
+import '../styles/css/Login.css' ;
 export default function Login() {
 
     return (
