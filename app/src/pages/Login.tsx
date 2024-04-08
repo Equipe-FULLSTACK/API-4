6eefa320e773010be41070a2c0964b468edb06e8
Willians Motta <112169854+williansmott4@users.noreply.github.com>
2024-04-08 00:17:32 -0300

    pt1 style login

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -4,45 +4,43 @@ import '../styles/css/Login.css' ;
 export default function Login() {
 
     return (
-        <div className="login-container" id="fundo">
-        <div className="login-imagem">
-            <img src="/./icons/logo_ionic_health.png" alt="" />
+        <div className = "login-container" >
+        <div className = "login-imagem">
+        <img src       = "/./icons/logo_ionic_health.png" alt = "" />
         </div>
-        <div className="login-form">
-            <div className="login-header-main">
-                Bem Vindo
+        <div className = "login-form">
+            
+            <div className = "login-header-second">
+               
             </div>
-            <div className="login-header-second">
-                Acesso sistema de gerenciamento de processos e tarefas da empresa
-            </div>
-            <form className="login-form-container" method='post' >
+            <form className = "login-form-container" method = 'post' >
 
-                <div className="login-form-container">
-                    <p>Login</p>
-                </div>
-
-                <input className="login-input"
-                    type="text"
+                <div className = "login-form-container">
                     
-                    autoComplete='off'
-                    name="username"
-                    id="username"
-                    placeholder="Login" />
-
-                <div className="login-form-container">
-                    <p>Senha</p>
                 </div>
 
-                <input type="password"
-                    name="senha"
+                    <input className = "login-input"
+                           type      = "text"
                     
-                    id="senha"
-                    placeholder="Senha" />
+                    autoComplete = 'off'
+                    name         = "username"
+                    id           = "username"
+                    placeholder  = "E-mail" />
 
-                <div className="login-form-recuperar">
-                    <a className="login-form-recuperar" href="/recuperar_senha">Esqueceu sua senha?</a>
+                <div className = "login-form-container">
+                    
                 </div>
-                <button type="button" >Entrar</button>
+
+                <input type = "password"
+                       name = "senha"
+                    
+                    id          = "senha"
+                    placeholder = "Senha" />
+
+                <div className = "login-form-recuperar">
+                <a   className = "login-form-recuperar" href = "/recuperar_senha">Esqueceu sua senha?</a>
+                </div>
+                <button type = "button" >Entrar</button>
 
             </form>
 
