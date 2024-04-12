24c072317dbf99c177a32059083da1c211b0b9bc
Pedro <pz020609@gmail.com>
2024-04-12 16:59:23 -0300

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ var con = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "fatec",
-	database: "db"
+	database: "siatt"
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,6 +42,7 @@ app.use(cors({
 	credentials: true
 }));
 
+/*
 app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(session({
@@ -53,10 +54,10 @@ app.use(session({
 		maxAge: 1000 * 60 * 60 * 24 //Tempo em milisegundos que o cookie vai durar
 	}
 }))
+*/
 
-
-// FUNÇÃO PARA RECEBER OS JSON DOS PROCESSOS GERAIS NA ROTA localhost:3000/
-
+/*
+// FUNÇÃO PARA RECEBER OS JSON DAS SALAS
 app.get("/", (req, res) => {
 	con.connect(function (err) {
 		if (err) throw err;
@@ -70,197 +71,16 @@ app.get("/", (req, res) => {
 		});
 	});
 });
-
-
-
-///// ROTA PARA COLETA DAS LOGS
-
-app.get("/logProcess", (req, res) => {
-	con.connect(function (err) {
-		if (err) throw err;
-		console.log("Dados solicitados para tabela de processos");
-
-		var sql = 'SELECT * FROM log_processes';
-		con.query(sql, req.url.substring(4), function (err, result, fields) {
-			if (err) throw err;
-			res.json(result);
-		});
-	});
-});
-
-app.get("/logTask", (req, res) => {
-	con.connect(function (err) {
-		if (err) throw err;
-		console.log("Dados solicitados para tabela de log test");
-
-		var sql = 'SELECT * FROM log_tasks';
-		con.query(sql, req.url.substring(4), function (err, result, fields) {
-			if (err) throw err;
-			res.json(result);
-		});
-	});
-});
-
-app.get("/log_users", (req, res) => {
-	con.connect(function (err) {
-		if (err) throw err;
-		console.log("Dados solicitados para tabela log usuários");
-
-		var sql = 'SELECT * FROM log_users';
-		con.query(sql, req.url.substring(4), function (err, result, fields) {
-			if (err) throw err;
-			res.json(result);
-		});
-	});
-});
-
-app.get("/log_roles", (req, res) => {
-	con.connect(function (err) {
-		if (err) throw err;
-		console.log("Dados solicitados para tabela log usuários");
-
-		var sql = 'SELECT * FROM log_roles';
-		con.query(sql, req.url.substring(4), function (err, result, fields) {
-			if (err) throw err;
-			res.json(result);
-		});
-	});
-});
-
-app.get("/log_evidences", (req, res) => {
-	con.connect(function (err) {
-		if (err) throw err;
-		console.log("Dados solicitados para tabela log usuários");
-
-		var sql = 'SELECT * FROM log_evidences';
-		con.query(sql, req.url.substring(4), function (err, result, fields) {
-			if (err) throw err;
-			res.json(result);
-		});
-	});
-});
-
-app.get("/log_template_processes", (req, res) => {
-	con.connect(function (err) {
-		if (err) throw err;
-		console.log("Dados solicitados para tabela log usuários");
-
-		var sql = 'SELECT * FROM log_template_processes';
-		con.query(sql, req.url.substring(4), function (err, result, fields) {
-			if (err) throw err;
-			res.json(result);
-		});
-	});
-});
-
-app.get("/log_template_processes", (req, res) => {
-	con.connect(function (err) {
-		if (err) throw err;
-		console.log("Dados solicitados para tabela log usuários");
-
-		var sql = 'SELECT * FROM log_template_processes';
-		con.query(sql, req.url.substring(4), function (err, result, fields) {
-			if (err) throw err;
-			res.json(result);
-		});
-	});
-});
-
-app.get("/log_template_evidences", (req, res) => {
-	con.connect(function (err) {
-		if (err) throw err;
-		console.log("Dados solicitados para tabela log usuários");
-
-		var sql = 'SELECT * FROM log_template_evidences';
-		con.query(sql, req.url.substring(4), function (err, result, fields) {
-			if (err) throw err;
-			res.json(result);
-		});
-	});
-});
-
-
-
-
-
-// FUNÇÃO PARA CRIAR NOVAS TAREFAS NO BANCO DE DADOS
-app.post('/t', function (req, res) {
-	const process = req.body.process;
-	const active = req.body.active;
-	const status = req.body.status;
-	const name = req.body.name;
-	const deadline = req.body.deadline;
-	const description = req.body.description;
-
-	const { authorization } = true;
-
-	if (authorization) {
-		con.connect(function (err) {
-			if (err) throw err;
-			console.log("Inserindo tarefas no banco de dados");
-
-			var sql = `INSERT INTO tasks (process, active, status, name, created, deadline, description) VALUES (${process}, ${active}, ${status}, ${name}, NOW(), ${deadline}, ${description})`
-			con.query(sql, [task.name, task.deadline, task.description], function (err, result) {
-				if (err) throw err;
-				res.status(200).send({ message: "Tarefa criada com sucesso!" });
-			});
-		});
-	} else {
-		res.status(400).send({ message: "Falha de autenticação verificar credenciais" });
-	}
-});
-
-
-
-
-app.put('/t/:id', function (req, res) {
-	const taskId = req.params.id;
-	const updatedTaskData = req.body;
-	const { process, active, status, name, deadline, description } = updatedTaskData;
-	const { authorization } = req.headers;
-
-
-
-	if (authorization) {
-		con.connect(function (err) {
-			if (err) throw err;
-			console.log("Atualizando tarefa no banco de dados");
-
-			var sql = `UPDATE tasks SET process = ?, active = ?, status = ?, name = ?, deadline = ?, description = ? WHERE id = ?`;
-			con.query(sql, [process, active, status, name, deadline, description, taskId], function (err, result) {
-				if (err) throw err;
-
-				res.status(200).send({ message: "Tarefa atualizada com sucesso!" });
-			});
-		});
-	} else {
-		res.status(400).send({ message: "O nome e o prazo são obrigatórios!" });
-	}
-});
-
+*/
 
 
 // FUNÇÃO PARA RECEBER OS JSON DOS USUARIOS NA ROTA localhost:3000/us
-
 app.get("/us", (req, res) => {
 	con.connect(function (err) {
 		if (err) throw err;
 		console.log("Conectado Usuarios");
 
-		var sql = 'SELECT id, admin, name, email, role FROM users';
-		con.query(sql, function (err, result, fields) {
-			if (err) throw err;
-			res.json(result);
-		});
-	});
-});
-
-app.get("/roles", (req, res) => {
-	con.connect(function (err) {
-		if (err) throw err;
-		console.log("Conectado Roles");
-
-		var sql = 'SELECT id, name FROM roles';
+		var sql = 'SELECT * FROM usuarios';
 		con.query(sql, function (err, result, fields) {
 			if (err) throw err;
 			res.json(result);
@@ -271,17 +91,17 @@ app.get("/roles", (req, res) => {
 
 // FUNÇÃO QUE FAZ O CADASTRO DE NOVOS USUÁRIOS NO BANCO DE DADOS
 app.post('/register', function (req, res) {
-	const { login, apelido, email, senha, tel } = req.body;
-	console.log(`recebidos os dados: ${login}, ${apelido}, ${email}, ${senha}, ${tel}`)
-	if (login != "" && apelido != "" && email != "" && senha != "" && tel != "") {
+	const { nome, email, diretoria, permissao, senha, admin } = req.body;
+	console.log(`recebidos os dados: ${nome}, ${email}, ${diretoria}, ${permissao}, ${senha}, ${admin}`)
+	if (nome != "" && email != "" && diretoria != "" && permissao != "" && senha != "") {
 		con.connect(function (err) {
 			if (err) throw err;
 			console.log("Inserindo");
-			var sql = 'INSERT INTO users (admin, name, nickname, password, phone, email, image, role) VALUES (0, ?, ?, ?, ?, ?, "", 4)';
-			con.query(sql, [login, apelido, senha, tel, email ], function (err, result) {
+		var sql = 'INSERT INTO usuarios (nome, email, diretoria, permissao, senha, admin) VALUES (?, ?, ?, ?, ?, 0)';
+			con.query(sql, [nome, email, diretoria, permissao, senha, admin], function (err, result) {
 				if (err) throw err;
 			});
-			console.log("usuario cadastrado com sucesso")
+			console.log("Usuario cadastrado com sucesso")
 		});
 	} else {
 		console.log("Erro");
@@ -289,35 +109,7 @@ app.post('/register', function (req, res) {
 });
 
 
-// FUNÇÃO QUE CHECA SE O USUÁRIO E SUA SENHA CONSTAM NO BANCO DE DADOS PARA FAZER O LOGIN
-app.post('/login', function (req, res) {
-	const { username, senha } = req.body; // Use req.body para acessar os valores enviados no corpo da solicitação
-
-	con.connect(function (err) {
-		if (err) throw err;
-
-		console.log("Verificando Cadastro");
-		const sql = 'SELECT * FROM users WHERE name = ? AND password = ?';
-		con.query(sql, [username, senha], (err, result) => {
-			if (err) return res.json({ Message: "Erro no servidor" });
-			if (result.length > 0) {
-				console.log(username);
-				req.session.username = result[0].name;
-				req.session.admin = result[0].admin;
-				req.session.role = result[0].role; 
-
-				console.log(req.session.username, 'username', 'Admin: ', req.session.admin,' Role: ', req.session.role)
-				return res.json({ Login: true, username: req.session.username, admin: req.session.admin, role:req.session.role });
-			} else {
-				console.log('Usuário não encontrado', username, senha);
-				return res.json({ Login: false });
-			}
-		});
-	});
-});
-
-
-//FUNÇÃO PARA A CHECAGEM DE COOKIES DE USUARIO
+/*
 app.get("/ck", (req, res) => {
 	con.connect(function (err) {
 		if (err) throw err;
@@ -332,30 +124,9 @@ app.get("/ck", (req, res) => {
 		}
 	});
 });
+*/
 
-//FUNÇÃO PARA ATUALIZAR A ROLE DE USUARIO
-app.put('/atualizarRole/:userId', (req, res) => {
-    const { userId } = req.params;
-    const { role } = req.body;
-
-	console.log('Recebido PUT request:', req.params, req.body);
-
-    // Execute a consulta SQL para atualizar o campo 'role' para o usuário com o userId.
-    var sql = 'UPDATE users SET role = ? WHERE id = ?';
-
-    con.query(sql, [role, userId], (err, result) => {
-        if (err) {
-            console.error('Erro ao atualizar a role:', err);
-            res.status(500).json({ message: 'Erro ao atualizar a role' });
-        } else {
-			console.log('Atualização efetuada')
-            res.json({ message: 'Role atualizada com sucesso' });
-        }
-    });
-});
-
-
-////// FUNÇÕES ORIGINAIS
+/*
 //FUNÇÕES DE INSERT
 // FUNÇÃO PARA CRIAR NOVOS PROCESSOS NO BANCO DE DADOS
 
@@ -532,27 +303,7 @@ app.post('/deleteevidence', function (req, res) {
 	}
 	return res.redirect('http://localhost:5173/processos');
 });
-
-//UPLOAD DE ARQUIVOS
-var location = "";
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		location = "";
-		cb(null, 'uploads/');
-	},
-	filename: (req, file, cb) => {
-		cb(null, Date.now() + '-' + file.originalname);
-		location = Date.now() + '-' + file.originalname;
-	}
-});
-const upload = multer({ storage: storage });
-
-module.exports = upload;
-
-//UPLOAD DE ARQUIVOS
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.json({ message: location });
-});
+*/
 
 app.listen(PORT, () => {
 	console.log(`Servidor recebendo dados no port ${PORT}`);
